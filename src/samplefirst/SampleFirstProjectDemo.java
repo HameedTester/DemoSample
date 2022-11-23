b2037257e1aef0a7b1a05600d5811be615742089
HameedTester <hameedahamed04@gmail.com>
2022-11-23 08:55:34 +0530

    demo sample

---
+++ src/samplefirst/SampleFirstProjectDemo.java
@@ -0,0 +1,13 @@
+package samplefirst;
+
+public class SampleFirstProjectDemo {
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hello Java!!!!!!!!!!!!!!!!!");
+		
+	}
+
+}
